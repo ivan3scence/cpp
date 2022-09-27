28fbb6f6f2af64a2a4dcf74c7209d08e478914c9
Superintendent Donny <sdonny@student.21-school.ru>
2022-09-27 19:47:37 +0300

    cpp01/00

--- cpp01/ex00/src/main.cpp
+++ cpp01/ex00/src/main.cpp
@@ -9,11 +9,27 @@ int	main(void)
 	z2 = newZombie("kek");
 	randomChump("dead");
 	z2->announce();
+	delete z2;
 	z2 = &z3;
 	z2->announce();
-	for (int i=0; i < 1000000; ++i)
+	for (int i=0; i > -1; ++i)
 	{
 		std::cout << i << ": ";
-		randomChump("gg");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		z2 = newZombie("zombie2");
+		randomChump("zombie");
 	}
 }
--- cpp01/ex00/src/randomChump.cpp
+++ cpp01/ex00/src/randomChump.cpp
@@ -6,4 +6,5 @@ void randomChump(std::string name)
 
 	zombie = newZombie(name);
 	zombie->announce();
+	delete zombie;
 }
